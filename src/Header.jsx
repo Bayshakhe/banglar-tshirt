ccb5e51ac0b77f4ef09f6832dda9462d7e551ff5
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-04-06 21:45:28 +0600

    basic setup done

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='bg-violet-300 font-bold'>
+            <Link className='mr-8' to='/'>Home</Link>
+            <Link className='mr-8' to='/order'>Order</Link>
+            <Link className='mr-8' to='/about'>About</Link>
+            <Link className='mr-8' to='/contact'>Contact</Link>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Home from "./Home";
+import Order from "./Order";
+import About from "./About";
+import Contact from "./contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        loader: () => fetch("tshirts.json")
+      },
+      {
+        path: "/order",
+        element: <Order></Order>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
